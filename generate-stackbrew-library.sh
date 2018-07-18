444e8f64e3ed70fce18025ba2a503daa6010a6ae
Tianon Gravi <admwiggin@gmail.com>
2018-07-18 10:57:57 -0700

    Add initial "generate-stackbrew-library.sh"

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+self="$(readlink -f "$BASH_SOURCE")"
+dir="$(dirname "$self")"
+cd "$dir"
+
+gitHubUrl='https://github.com/tianon/docker-brew-cirros'
+gitFetch='refs/heads/dist'
+
+commit="$(git ls-remote "$gitHubUrl.git" "$gitFetch" | cut -d$'\t' -f1)"
+
+version="$(curl -fsSL "$gitHubUrl/raw/$commit/arches/version")"
+arches="$(curl -fsSL "$gitHubUrl/raw/$commit/arches/supported-arches")"
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+selfCommit="$(git log --format='format:%H' -1 "$self")"
+cat <<-EOH
+# this file is generated via $gitHubUrl/blob/$selfCommit/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon)
+GitRepo: $gitHubUrl.git
+GitFetch: $gitFetch
+GitCommit: $commit
+Architectures: $(join ', ' $arches)
+EOH
+for arch in $arches; do
+	echo "$arch-Directory: arches/$arch"
+done
+
+tags=()
+while [ "${version%[.-]*}" != "$version" ]; do
+	tags+=( $version )
+	version="${version%[.-]*}"
+done
+tags+=(
+	$version
+	latest
+)
+
+echo
+echo "Tags: $(join ', ' "${tags[@]}")"
